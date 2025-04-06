0652cf80888e9fcc53917c7fc397998351834a87
Adamiscool123 <adamidrissi177@gmail.com>
2025-04-06 16:35:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name='Sheet 1')
+    df = pd.read_excel(filepath)
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     filename = Path(filepath).stem
